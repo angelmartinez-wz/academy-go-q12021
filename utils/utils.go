b7c533b7a0e120edcf01df4a5b5a3dbe87018152
Angel Martínez <angel.martinez@wizeline.com>
2021-03-05 10:42:28 -0600

    Fix identation for readcsv function

--- utils/utils.go
+++ utils/utils.go
@@ -65,39 +65,41 @@ func ReadCSV() (model.PokemonList, error) {
 	var pokemonList model.PokemonList
 	recordFile, err := os.Open("assets/pokemon.csv")
 
-	if err == nil {
-		reader := csv.NewReader(recordFile)
-		records, err := reader.ReadAll()
-
-		if err == nil {
-			for _, pokemon := range records {
-				id, err := strconv.Atoi(pokemon[0])
-		
-				if err != nil {
-					 return pokemonList, errors.New("Cannot get id from row")
-				}
-				
-				pkNumber, err := strconv.Atoi(pokemon[1])
-		
-				if err != nil {
-					return pokemonList, errors.New("Cannot get pokedex number from row")
-				}
-		
-				pk := model.Pokemon{Id:id, PokedexNumber: pkNumber, Name:pokemon[2], Types:pokemon[3], Region:pokemon[4]}
-				pokemonList = append(pokemonList, pk)
-			}
-		
-			err = recordFile.Close()
-		
-			if err != nil {
-				return pokemonList, errors.New("Error while closing file")
-			}
-		
-			return pokemonList, nil
-		}
+	if err != nil {
+		return nil, errors.New("Could not open file")
 	}
 
-	return nil, err
+	reader := csv.NewReader(recordFile)
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		return nil, errors.New("Could not read file")
+	}
+
+	for _, pokemon := range records {
+		id, err := strconv.Atoi(pokemon[0])
+		
+		if err != nil {
+			return pokemonList, errors.New("Cannot get id from row")
+		}
+				
+		pkNumber, err := strconv.Atoi(pokemon[1])
+		
+		if err != nil {
+			return pokemonList, errors.New("Cannot get pokedex number from row")
+		}
+		
+		pk := model.Pokemon{Id:id, PokedexNumber: pkNumber, Name:pokemon[2], Types:pokemon[3], Region:pokemon[4]}
+		pokemonList = append(pokemonList, pk)
+	}
+		
+	err = recordFile.Close()
+		
+	if err != nil {
+		return pokemonList, errors.New("Error while closing file")
+	}
+		
+	return pokemonList, nil
 }
 
 func getFieldString(pokemon *model.Pokemon, field string) string {
